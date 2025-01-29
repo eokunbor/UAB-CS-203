dc0e3a134fa7f0cf747fc418d3db789fb82a9558
Elizabeth Okunbor <elizabethokunbor04@gmail.com>
2025-01-29 10:31:35 -0600

    update cloud

---
+++ CS 203 Codes/Lab 3/UABPerson.java
@@ -0,0 +1,41 @@
+
+public class UABPerson {
+	private String name;
+    private String gender;
+    private int age;
+    private String blazer;
+
+    public UABPerson (String name, String gender, int age, String blazer) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.blazer = blazer;
+    }
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getBlazer() {
+		return blazer;
+	}
+	public void setBlazer(String blazer) {
+		this.blazer = blazer;
+	}
+    
+    
+}
