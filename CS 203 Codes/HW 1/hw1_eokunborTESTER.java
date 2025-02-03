e1de1f5a43428bbc84f73dd91b4f7cfadd36115c
Elizabeth Okunbor <elizabethokunbor04@gmail.com>
2025-02-02 23:14:31 -0600

    update

--- CS 203 Codes/HW 1/hw1_eokunborTESTER.java
+++ CS 203 Codes/HW 1/hw1_eokunborTESTER.java
@@ -1,3 +1,25 @@
 public class hw1_eokunborTESTER {
-    
+    public static void main(String[] args) {
+        double bmi1 = calculateBMI(70, 1.75);
+        double bmi2 = calculateBMI(85, 1.80);
+        System.out.println("BMI 1: " + bmi1);
+        System.out.println("BMI 1: " + bmi2);
+
+        boolean result1 = blazerNumber(28);
+        boolean result2 = blazerNumber(12);
+        System.out.println("Blazer Number 1: " + result1);
+        System.out.println("Blazer Number 2: " + result2);
+
+        int prime1 = findLargestPrime(20);
+        int prime2 = findLargestPrime(50);
+        System.out.println("Largest Prime 1: " + prime1);
+        System.out.println("Largest Prime 2: " + prime2);
+
+        int steps = numSteps(14);
+        System.out.println("Num Steps: " + prime1);
+
+        countVowelsConsonants("Hello");
+        countVowelsConsonants("programming");
+
+    }
 }
